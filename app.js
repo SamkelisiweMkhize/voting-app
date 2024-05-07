e2273fa4413d0a8feda721c4095ea355cb446402
Samkelisiwe Mkhize <samkelsiwemkhize@gmail.com>
2024-05-07 09:01:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function info() {
+let age = document.getElementById('age')
+let = result = document.getElementById('result')
+let = nationality = document.getElementById('nationality')
+
+if (age.value >17 && "South African") {
+result.innerHTML ='You can vote'
+} 
+else {
+    result.innerHTML ='You can not vote'
+}
+}
